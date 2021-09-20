2bb6941f5b0ffb3213f69ec8c81f3f025a305650
rahulsingh3292 <60551261+rahulsingh3292@users.noreply.github.com>
2021-09-20 15:15:00 +0530

    Update urls.py

--- LibraryManagement/urls.py
+++ LibraryManagement/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("library.urls")),
 ] + static(settings.STATIC_URL,document_root =settings.STATIC_ROOT)
-tic(settings.ST
+tic(settingssettisettingssesettingss
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
