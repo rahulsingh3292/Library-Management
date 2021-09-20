ee3be5d68c2fbc2422f6cef899176b24fb7411e8
rahulsingh3292 <rahulsingh32929@gmail.com>
2021-09-20 14:45:15 +0530

    initial

--- LibraryManagement/urls.py
+++ LibraryManagement/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path("",include("library.urls")),
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
