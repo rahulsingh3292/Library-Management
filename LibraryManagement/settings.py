5a08b7521a9edf9859710bc1a79e4b4ed324c81d
rahulsingh3292 <rahulsingh32929@gmail.com>
2021-09-20 12:39:57 +0530

    dp

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z_itb!$i%xbnrvvxbwrvfjg0qxn9quik8p(rm*3r%(0p#@^ho_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["libraryappnew.herokuapp.com"]
 
 
 # Application definition
